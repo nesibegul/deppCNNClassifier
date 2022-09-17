b46f7268a1d1c78b33df58781f7b62b4b0326a18
nesibegul <nesibegl@gmail.com>
2022-09-17 22:04:53 +0300

    basicSkeleton

---
+++ template.py
@@ -0,0 +1,46 @@
+from asyncio.log import logger
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format=' [%(asctime)s]: %(message)s: ')
+
+print(Path("x/y/a.txt"))
+package_name = "deppClassifier"
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/config/__init__.py",
+    f"src/{package_name}/pipeline/__init__.py",
+    f"src/{package_name}/entity/__init__.py",
+    f"src/{package_name}/constants/__init__.py",
+    "configs/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb",
+    "example.py"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass #this helps create a empty file
+            logger.info(f"Creating file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists ")
